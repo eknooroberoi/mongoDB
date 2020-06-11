78b87a054133e5559f4eb9e9d096f2851a59ced7
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-06-11 20:41:01 +0530

    Data Validation with Mongoose

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ mongoose.connect("mongodb://localhost:27017/fruitDB", {useNewUrlParser: true, us
 //if fruitDB not present, it will create one
 const fruitSchema = new mongoose.Schema(
   {
-    name: String,
-    rating: Number,
+    name: {
+      type: String,
+      required: [true, "Please check your data entry, no name specified"]
+    },
+    rating: {
+      type: Number,
+      min:1,
+      max: 10
+    },
     review: String
   }
 );
 const Fruit= mongoose.model("Fruit", fruitSchema);
 const fruit= new Fruit(
   {
-    name: "Apple",
-    rating: 7,
-    review: "Pretty solid as a fruit."
+    // name: "Apple",
+    rating: 34,
+    review: "Yummy peaches!"
   }
 );
 
-//fruit.save();//remember that every single time you run fruit.save it will save the same fruit into your fruits collection in your fruits database.
+fruit.save();//remember that every single time you run fruit.save it will save the same fruit into your fruits collection in your fruits database.
 //So if you don't want that to happen, then you can just comment out this method call and you won't end up with a whole bunch of apples in your database.
 
 const personSchema= new mongoose.Schema(
@@ -37,54 +44,54 @@ const person = new Person(
 
 //person.save();
 
-const kiwi = new Fruit(
-  {
-    name: "Kiwi",
-    score: 10,
-    review: "The best fruit!"
-  }
-);
-const orange = new Fruit(
-  {
-    name: "Orange",
-    score: 4,
-    review: "Too sour for me"
-  }
-);
-const banana= new Fruit(
-  {
-    name: "Banana",
-    score: 3,
-    review: "Weird texture"
-  }
-);
-
-
-
-//insertMany and it takes two parameters. The first one is an array of objects that match that particular schema,
-//so all fruits here. We've got kiwi, orange and bananas. So we'll add that kiwi, orange and banana.
-//And then the second parameter will be a callback and it allows us to log any errors
-//if there were any issues with inserting all of these objects into our fruits collection.
-//And so we can check say if there's an error then we'll log the error, but else then we'll log "Successfully
-//saved all the fruits to fruitsDB". And let's close that off and let's hit save and exit out of our database
-//and run our app.js again.
-Fruit.insertMany([kiwi,orange,banana], function(err)
-{
-  if(err){
-    console.log(err);
-  }else{
-    console.log("Successfully saved all the fruits to fruitsDB");
-  }
-});
-
-
-// Fruit.find(function(err, fruits){
+// const kiwi = new Fruit(
+//   {
+//     name: "Kiwi",
+//     score: 10,
+//     review: "The best fruit!"
+//   }
+// );
+// const orange = new Fruit(
+//   {
+//     name: "Orange",
+//     score: 4,
+//     review: "Too sour for me"
+//   }
+// );
+// const banana= new Fruit(
+//   {
+//     name: "Banana",
+//     score: 3,
+//     review: "Weird texture"
+//   }
+// );
+//
+//
+//
+// //insertMany and it takes two parameters. The first one is an array of objects that match that particular schema,
+// //so all fruits here. We've got kiwi, orange and bananas. So we'll add that kiwi, orange and banana.
+// //And then the second parameter will be a callback and it allows us to log any errors
+// //if there were any issues with inserting all of these objects into our fruits collection.
+// //And so we can check say if there's an error then we'll log the error, but else then we'll log "Successfully
+// //saved all the fruits to fruitsDB". And let's close that off and let's hit save and exit out of our database
+// //and run our app.js again.
+// Fruit.insertMany([kiwi,orange,banana], function(err)
+// {
 //   if(err){
 //     console.log(err);
 //   }else{
-//     //mongoose.connection.close();
-//     fruits.forEach(function(fruit){
-//       console.log(fruit.name);
-//     });
+//     console.log("Successfully saved all the fruits to fruitsDB");
 //   }
 // });
+
+
+Fruit.find(function(err, fruits){
+  if(err){
+    console.log(err);
+  }else{
+    //mongoose.connection.close();
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    });
+  }
+});
